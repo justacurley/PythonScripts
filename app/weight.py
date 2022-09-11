9af0bd7985af540f0055fb31a81ddf2891f68454
Alex Curley <acurley@loandepot.com>
2022-09-10 18:47:39 -0600

    Working virtual environment and flask app

---
+++ app/weight.py
@@ -0,0 +1,16 @@
+# before running, set the FLASK_APP env var to the basename of this file $ENV:FLASK_APP="weight"
+from datetime import datetime
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def form():
+    return render_template('form.html')
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'POST':
+        return render_template(
+            'addweight.html',
+            weight=request.form['weight'],
+            date=datetime.now()
+        )
