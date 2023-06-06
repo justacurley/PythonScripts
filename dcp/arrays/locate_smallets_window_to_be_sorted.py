32ec8aa91dc70cba0e8f54f7d982e40aea79bef5
Alex Curley <alexwmcurley@gmail.com>
2023-06-06 17:14:12 -0600

    Add more stuff

--- dcp/arrays/locate_smallets_window_to_be_sorted.py
+++ dcp/arrays/locate_smallets_window_to_be_sorted.py
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 # Given an array of integers that are out of order, determine the bounds of the smallest window that must be sorted in order for the entire array to be sorted.
 # Input [2,7,5,6,9]
 # Output 1,3
 
 numbreros = [2,7,5,6,9]
+=======
+>>>>>>> parent of 591d334 (Add more problem questions)
